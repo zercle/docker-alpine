f1acb07509ac73a97c88d0fda19c6a0d4cb6af36
Kawin Viriyaprasopsook <bouroo@gmail.com>
2017-07-15 01:44:55 +0700

    Use libressl

--- files/root/entrypoint.sh
+++ files/root/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-openssl dhparam -dsaparam -out /etc/ssl/dhparam.pem 4096
+if [ ! -e /etc/ssl/dhparam.pem ]; then
+libressl dhparam -out /etc/ssl/dhparam.pem 2048
+fi
 
 service sshd restart
 
